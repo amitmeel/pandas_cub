0015171e1f5f0ad197ec03f4bb42793b6e5d22ef
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 17:35:37 +0530

    filter dataframe testing

--- pandas_cub/tests/test_dataframe.py
+++ pandas_cub/tests/test_dataframe.py
@@ -92,3 +92,107 @@ class TestDataFrameCreation:
         df_answer = pdc.DataFrame({'Column Name': cols,
                                    'Data Type': dtypes})
         assert_df_equals(df_result, df_answer)
+
+
+class TestSelection:
+
+    def test_one_column(self):
+        assert_array_equal(df['a'].values[:, 0], a)
+        assert_array_equal(df['c'].values[:, 0], c)
+
+    def test_multiple_columns(self):
+        cols = ['a', 'c']
+        df_result = df[cols]
+        df_answer = pdc.DataFrame({'a': a, 'c': c})
+        assert_df_equals(df_result, df_answer)
+
+    def test_simple_boolean(self):
+        bool_arr = np.array([True, False, False])
+        df_bool = pdc.DataFrame({'col': bool_arr})
+        df_result = df[df_bool]
+        df_answer = pdc.DataFrame({'a': a[bool_arr], 'b': b[bool_arr], 
+                                   'c': c[bool_arr], 'd': d[bool_arr], 
+                                   'e': e[bool_arr]})
+        assert_df_equals(df_result, df_answer)
+
+        with pytest.raises(ValueError):
+            df_bool = pdc.DataFrame({'col': bool_arr, 'col2': bool_arr})
+            df[df_bool]
+
+        with pytest.raises(TypeError):
+            df_bool = pdc.DataFrame({'col': np.array[1, 2, 3]})
+
+    def test_one_column_tuple(self):
+        assert_df_equals(df[:, 'a'], pdc.DataFrame({'a': a}))
+
+    def test_multiple_columns_tuple(self):
+        cols = ['a', 'c']
+        df_result = df[:, cols]
+        df_answer = pdc.DataFrame({'a': a, 'c': c})
+        assert_df_equals(df_result, df_answer)
+
+    def test_int_selcetion(self):
+        assert_df_equals(df[:, 3], pdc.DataFrame({'d': d}))
+
+    def test_simultaneous_tuple(self):
+        with pytest.raises(TypeError):
+            s = set()
+            df[s]
+
+        with pytest.raises(ValueError):
+            df[1, 2, 3]
+
+    def test_single_element(self):
+        df_answer = pdc.DataFrame({'e': np.array([2])})
+        assert_df_equals(df[1, 'e'], df_answer)
+
+    def test_all_row_selections(self):
+        df1 = pdc.DataFrame({'a': np.array([True, False, True]),
+                             'b': np.array([1, 3, 5])})
+        with pytest.raises(ValueError):
+            df[df1, 'e']
+
+        with pytest.raises(TypeError):
+            df[df1['b'], 'c']
+
+        df_result = df[df1['a'], 'c']
+        df_answer = pdc.DataFrame({'c': c[[True, False, True]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df[[1, 2], 0]
+        df_answer = pdc.DataFrame({'a': a[[1, 2]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df[1:, 0]
+        assert_df_equals(df_result, df_answer)
+
+    def test_list_columns(self):
+        df_answer = pdc.DataFrame({'c': c, 'e': e})
+        assert_df_equals(df[:, [2, 4]], df_answer)
+        assert_df_equals(df[:, [2, 'e']], df_answer)
+        assert_df_equals(df[:, ['c', 'e']], df_answer)
+
+        df_result = df[2, ['a', 'e']]
+        df_answer = pdc.DataFrame({'a': a[[2]], 'e': e[[2]]})
+        assert_df_equals(df_result, df_answer)
+
+        df_answer = pdc.DataFrame({'c': c[[1, 2]], 'e': e[[1, 2]]})
+        assert_df_equals(df[[1, 2], ['c', 'e']], df_answer)
+
+        df1 = pdc.DataFrame({'a': np.array([True, False, True]),
+                             'b': np.array([1, 3, 5])})
+        df_answer = pdc.DataFrame({'c': c[[0, 2]], 'e': e[[0, 2]]})
+        assert_df_equals(df[df1['a'], ['c', 'e']], df_answer)
+
+    def test_col_slice(self):
+        df_answer = pdc.DataFrame({'a': a, 'b': b, 'c': c})
+        assert_df_equals(df[:, :3], df_answer)
+
+        df_answer = pdc.DataFrame({'a': a[::2], 'b': b[::2], 'c': c[::2]})
+        assert_df_equals(df[::2, :3], df_answer)
+
+        df_answer = pdc.DataFrame({'a': a[::2], 'b': b[::2], 'c': c[::2], 'd': d[::2], 'e': e[::2]})
+        assert_df_equals(df[::2, :], df_answer)
+
+        with pytest.raises(TypeError):
+            df[:, set()]
