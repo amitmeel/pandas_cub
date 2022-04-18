a68c082f1bbf349ea263fc2fcfcbe778700da5af
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 19:35:31 +0530

    Test case for arithmatic and compariosn operators

--- pandas_cub/tests/test_dataframe.py
+++ pandas_cub/tests/test_dataframe.py
@@ -446,6 +446,94 @@ class TestNonAgg:
         assert_df_equals(df_result, df_answer)
 
 
+a5 = np.array([11, 5])
+b5 = np.array([3.4, 5.1])
+df5 = pdc.DataFrame({'a': a5, 'b': b5})
+
+
+class TestOperators:
+
+    def test_add(self):
+        df_result = df5 + 3
+        df_answer = pdc.DataFrame({'a': a5 + 3, 'b': b5 + 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 3 + df5
+        assert_df_equals(df_result, df_answer)
+
+    def test_sub(self):
+        df_result = df5 - 3
+        df_answer = pdc.DataFrame({'a': a5 - 3, 'b': b5 - 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 3 - df5
+        df_answer = pdc.DataFrame({'a': 3 - a5, 'b': 3 - b5})
+        assert_df_equals(df_result, df_answer)
+
+    def test_mul(self):
+        df_result = df5 * 3
+        df_answer = pdc.DataFrame({'a': a5 * 3, 'b': b5 * 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 3 * df5
+        assert_df_equals(df_result, df_answer)
+
+    def test_truediv(self):
+        df_result = df5 / 3
+        df_answer = pdc.DataFrame({'a': a5 / 3, 'b': b5 / 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 3 / df5
+        df_answer = pdc.DataFrame({'a': 3 / a5, 'b': 3 / b5})
+        assert_df_equals(df_result, df_answer)
+
+    def test_floordiv(self):
+        df_result = df5 // 3
+        df_answer = pdc.DataFrame({'a': a5 // 3, 'b': b5 // 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 3 // df5
+        df_answer = pdc.DataFrame({'a': 3 // a5, 'b': 3 // b5})
+        assert_df_equals(df_result, df_answer)
+
+    def test_pow(self):
+        df_result = df5 ** 3
+        df_answer = pdc.DataFrame({'a': a5 ** 3, 'b': b5 ** 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = 2 ** df5
+        df_answer = pdc.DataFrame({'a': 2 ** a5, 'b': 2 ** b5})
+        assert_df_equals(df_result, df_answer)
+
+    def test_gt_lt(self):
+        df_result = df5 > 3
+        df_answer = pdc.DataFrame({'a': a5 > 3, 'b': b5 > 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df5 < 2
+        df_answer = pdc.DataFrame({'a': a5 < 2, 'b': b5 < 2})
+        assert_df_equals(df_result, df_answer)
+
+    def test_ge_le(self):
+        df_result = df5 >= 3
+        df_answer = pdc.DataFrame({'a': a5 >= 3, 'b': b5 >= 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df5 < 2
+        df_answer = pdc.DataFrame({'a': a5 <= 2, 'b': b5 <= 2})
+        assert_df_equals(df_result, df_answer)
+
+    def test_eq_ne(self):
+        df_result = df5 == 3
+        df_answer = pdc.DataFrame({'a': a5 == 3, 'b': b5 == 3})
+        assert_df_equals(df_result, df_answer)
+
+        df_result = df5 != 2
+        df_answer = pdc.DataFrame({'a': a5 != 2, 'b': b5 != 2})
+        assert_df_equals(df_result, df_answer)
+
+
+
 a8 = np.array(['b', 'a', 'a', 'a', 'b', 'a', 'a', 'b'])
 b8 = np.array(['B', 'A', 'A', 'A', 'B', 'B', 'B', 'A'])
 c8 = np.array([1, 2, 3, 4, 5, 6, 7, 8])
