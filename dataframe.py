2c3d39aa08791f7e415ba2617c89a42aa7446c76
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 12:10:28 +0530

    Empty dataframe class defined

---
+++ dataframe.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class DataFrame:
+
+    def __init__(self, data):
+        """
+        A DataFrame holds two dimensional heterogeneous data. Create it by
+        passing a dictionary of NumPy arrays to the values parameter
+
+        Parameters
+        ----------
+        data: dict
+            A dictionary of strings mapped to NumPy arrays. The key will
+            become the column name.
+        """
+        pass
