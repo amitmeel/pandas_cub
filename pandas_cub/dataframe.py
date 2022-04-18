ad923c891cf220f371b9fa4dc9f716af000f1eab
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 14:41:59 +0530

    Special method for strings added

--- pandas_cub/dataframe.py
+++ pandas_cub/dataframe.py
@@ -23,6 +23,11 @@ class DataFrame:
         # convert unicode arrays to object
         self._data = self._convert_unicode_to_object(data)
 
+        # Allow for special methods for strings
+        self.str = StringMethods(self)
+        self._add_docs()
+
+
     def _check_input_types(self, data):
         """
         Check that the input data is of the correct type.
@@ -55,3 +60,121 @@ class DataFrame:
             if values.dtype.kind == 'U':
                 data[column_name] = values.astype(object)
         return data
+
+    def _add_docs(self):
+        agg_names = ['min', 'max', 'mean', 'median', 'sum', 'var',
+                     'std', 'any', 'all', 'argmax', 'argmin']
+        agg_doc = \
+        """
+        Find the {} of each column
+
+        Returns
+        -------
+        DataFrame
+        """
+        for name in agg_names:
+            getattr(DataFrame, name).__doc__ = agg_doc.format(name)
+
+class StringMethods:
+
+    def __init__(self, df):
+        self._df = df
+
+    def capitalize(self, col):
+        return self._str_method(str.capitalize, col)
+
+    def center(self, col, width, fillchar=None):
+        if fillchar is None:
+            fillchar = ' '
+        return self._str_method(str.center, col, width, fillchar)
+
+    def count(self, col, sub, start=None, stop=None):
+        return self._str_method(str.count, col, sub, start, stop)
+
+    def endswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.endswith, col, suffix, start, stop)
+
+    def startswith(self, col, suffix, start=None, stop=None):
+        return self._str_method(str.startswith, col, suffix, start, stop)
+
+    def find(self, col, sub, start=None, stop=None):
+        return self._str_method(str.find, col, sub, start, stop)
+
+    def len(self, col):
+        return self._str_method(str.__len__, col)
+
+    def get(self, col, item):
+        return self._str_method(str.__getitem__, col, item)
+
+    def index(self, col, sub, start=None, stop=None):
+        return self._str_method(str.index, col, sub, start, stop)
+
+    def isalnum(self, col):
+        return self._str_method(str.isalnum, col)
+
+    def isalpha(self, col):
+        return self._str_method(str.isalpha, col)
+
+    def isdecimal(self, col):
+        return self._str_method(str.isdecimal, col)
+
+    def islower(self, col):
+        return self._str_method(str.islower, col)
+
+    def isnumeric(self, col):
+        return self._str_method(str.isnumeric, col)
+
+    def isspace(self, col):
+        return self._str_method(str.isspace, col)
+
+    def istitle(self, col):
+        return self._str_method(str.istitle, col)
+
+    def isupper(self, col):
+        return self._str_method(str.isupper, col)
+
+    def lstrip(self, col, chars):
+        return self._str_method(str.lstrip, col, chars)
+
+    def rstrip(self, col, chars):
+        return self._str_method(str.rstrip, col, chars)
+
+    def strip(self, col, chars):
+        return self._str_method(str.strip, col, chars)
+
+    def replace(self, col, old, new, count=None):
+        if count is None:
+            count = -1
+        return self._str_method(str.replace, col, old, new, count)
+
+    def swapcase(self, col):
+        return self._str_method(str.swapcase, col)
+
+    def title(self, col):
+        return self._str_method(str.title, col)
+
+    def lower(self, col):
+        return self._str_method(str.lower, col)
+
+    def upper(self, col):
+        return self._str_method(str.upper, col)
+
+    def zfill(self, col, width):
+        return self._str_method(str.zfill, col, width)
+
+    def encode(self, col, encoding='utf-8', errors='strict'):
+        return self._str_method(str.encode, col, encoding, errors)
+
+    def _str_method(self, method, col, *args):
+        old_values = self._df._data[col]
+        if old_values.dtype.kind != 'O':
+            raise TypeError('The `str` accessor only works with string columns')
+        new_values = []
+        for val in old_values:
+            if val is None:
+                new_values.append(val)
+            else:
+                new_val = method(val, *args)
+                new_values.append(new_val)
+        arr = np.array(new_values)
+        return DataFrame({col: arr})
