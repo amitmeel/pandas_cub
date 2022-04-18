161ac75effa45becd162d4e7fb8e4fc0799f41b3
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 20:41:28 +0530

    Test case for read_csv

--- pandas_cub/tests/test_dataframe.py
+++ pandas_cub/tests/test_dataframe.py
@@ -805,3 +805,39 @@ class TestStrings:
         answer = pdc.DataFrame({'movie': movie})
         assert_df_equals(result, answer)
 
+
+df_emp = pdc.read_csv('data/employee.csv')
+
+
+class TestReadCSV:
+
+    def test_columns(self):
+        result = df_emp.columns
+        answer = ['dept', 'race', 'gender', 'salary']
+        assert result == answer
+
+    def test_data_types(self):
+        df_result = df_emp.dtypes
+        cols = np.array(['dept', 'race', 'gender', 'salary'], dtype='O')
+        dtypes = np.array(['string', 'string', 'string', 'int'], dtype='O')
+        df_answer = pdc.DataFrame({'Column Name': cols,
+                                   'Data Type': dtypes})
+        assert_df_equals(df_result, df_answer)
+
+    def test_sum(self):
+        result = df_emp['salary'].sum()
+        answer = 86387875
+        assert result == answer
+
+    def test_head(self):
+        data = {'dept': np.array(['Houston Police Department-HPD',
+                                  'Houston Fire Department (HFD)',
+                                  'Houston Police Department-HPD',
+                                  'Public Works & Engineering-PWE',
+                                  'Houston Airport System (HAS)'], dtype='O'),
+                'race': np.array(['White', 'White', 'Black', 'Asian', 'White'], dtype='O'),
+                'gender': np.array(['Male', 'Male', 'Male', 'Male', 'Male'], dtype='O'),
+                'salary': np.array([45279, 63166, 66614, 71680, 42390])}
+        result = df_emp.head()
+        answer = pdc.DataFrame(data)
+        assert_df_equals(result, answer)
