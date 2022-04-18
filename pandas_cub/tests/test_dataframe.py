73bfd01ea14ddbe0689d43ed83ba8655c91ddc6d
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 13:42:17 +0530

    Test case for User input type check

---
+++ pandas_cub/tests/test_dataframe.py
@@ -0,0 +1,25 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+import pytest
+
+import pandas_cub as pdc
+
+class TestDataFrameCreation:
+    
+    def test_input_types(self):
+        with pytest.raises(TypeError):
+            pdc.DataFrame([1, 2, 3])
+
+        with pytest.raises(TypeError):
+            pdc.DataFrame({1: 5, 'b': 10})
+
+        with pytest.raises(TypeError):
+            pdc.DataFrame({'a': np.array([1]), 'b': 10})
+
+        with pytest.raises(ValueError):
+            pdc.DataFrame({'a': np.array([1]), 
+                           'b': np.array([[1]])})
+
+        # correct construction. no error
+        pdc.DataFrame({'a': np.array([1]), 
+                       'b': np.array([1])})
