b43c9afe69a4c3b4f161a87b6796193136107b48
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 13:41:46 +0530

    User input type check

--- pandas_cub/dataframe.py
+++ pandas_cub/dataframe.py
@@ -13,4 +13,22 @@ class DataFrame:
             A dictionary of strings mapped to NumPy arrays. The key will
             become the column name.
         """
-        pass
+        
+        # check for correct input types
+        self._check_input_types(data)
+
+    def _check_input_types(self, data):
+        """
+        Check that the input data is of the correct type.
+        """
+        if not isinstance(data, dict):
+            raise TypeError("`data` must be a dictionary of1-D NumPy arrays")
+
+        for column_name, values in data.items():
+            if not isinstance(column_name, str):
+                raise TypeError("All column names must be a string")
+            if not isinstance(values, np.ndarray):
+                raise TypeError("All values must be a 1-D NumPy array")
+            else:
+                if values.ndim != 1:
+                    raise ValueError("All values must be 1-D Numpy array")
