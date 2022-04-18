f8a2929305b0663aa3ceba55270e02506333a1cc
Amit Meel <32540863+amitmeel@users.noreply.github.com>
2022-04-18 14:29:56 +0530

    Check if two arrays are equal or not after unicode to obect conversion

--- pandas_cub/tests/test_dataframe.py
+++ pandas_cub/tests/test_dataframe.py
@@ -3,7 +3,18 @@ from numpy.testing import assert_array_equal
 import pytest
 
 import pandas_cub as pdc
+from ..tests import assert_df_equals
 
+pytestmark = pytest.mark.filterwarnings("ignore")
+
+import pandas_cub as pdc
+
+a = np.array(['a', 'b', 'c'])
+b = np.array(['c', 'd', None])
+c = np.random.rand(3)
+d = np.array([True, False, True])
+e = np.array([1, 2, 3])
+df = pdc.DataFrame({'a': a, 'b': b, 'c': c, 'd': d, 'e': e})
 class TestDataFrameCreation:
     
     def test_input_types(self):
@@ -31,3 +42,11 @@ class TestDataFrameCreation:
         # correct construction. no error                           
         pdc.DataFrame({'a': np.array([1, 2]), 
                         'b': np.array([5, 10])})
+
+    def test_convert_unicode_to_object(self):
+        a_object = a.astype('O')
+        assert_array_equal(df._data['a'], a_object)
+        assert_array_equal(df._data['b'], b)
+        assert_array_equal(df._data['c'], c)
+        assert_array_equal(df._data['d'], d)
+        assert_array_equal(df._data['e'], e)
